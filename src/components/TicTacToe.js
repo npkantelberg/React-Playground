d906ad63e4ff9e5d6c0a0300f66f0f65da8a5351
npkantelberg <npkantelberg@gmail.com>
2021-07-11 15:43:18 -0500

    tic tac toe functionality

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -1,28 +1,42 @@
 // import { array } from 'prop-types';
 import React from 'react';
 
-class Square extends React.Component { 
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClickProp()}>
-        {this.props.value}
-      </button>
-    );
-  }
+
+function Square(props){
+  return(
+    <button className="square" onClick={() => props.onClickProp()}>
+      {props.value}
+    </button>
+  )
 }
+
+// class Square extends React.Component { 
+//   render() {
+//     return (
+//       <button className="square" onClick={() => this.props.onClickProp()}>
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
 
 class Board extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true
     }
   }
 
   handleClick(i){
     const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({squares: squares})
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    console.log(squares[i]);
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    })
   }
 
   renderSquare(i) {
@@ -30,7 +44,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? "X" : "O");
 
     return (
       <div>
