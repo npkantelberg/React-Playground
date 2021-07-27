daf3051ed4923f02d6aa8eee0b48b64e6eb36d86
npkantelberg <npkantelberg@gmail.com>
2021-07-27 12:33:27 -0500

    finsihed game

--- src/components/TicTacToe.js
+++ src/components/TicTacToe.js
@@ -31,6 +31,9 @@ class Board extends React.Component {
 
   handleClick(i){
     const squares = this.state.squares.slice();
+    if(calculateWinner(squares) || squares[i]){
+      return;
+    }
     squares[i] = this.state.xIsNext ? "X" : "O";
     console.log(squares[i]);
     this.setState({
@@ -44,7 +47,15 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? "X" : "O");
+    // const status = 'Next player: ' + (this.state.xIsNext ? "X" : "O");
+
+    const winner = calculateWinner(this.state.squares);    
+    let status;    
+    if (winner) {
+      status = 'Winner: ' + winner;    
+    } else { 
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');    
+    }
 
     return (
       <div>
@@ -70,6 +81,7 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  
   render() {
     return (
       <div className="game">
@@ -93,3 +105,23 @@ class Game extends React.Component {
 // );
 
 export default Game;
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
